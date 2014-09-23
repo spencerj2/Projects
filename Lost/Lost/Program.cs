b54402108e43239f380ee28363f8e1bbeab65932
spencerj <vorpehd@gmail.com>
2014-09-23 08:19:56 -0400

    .
    ....

---
+++ Lost/Lost/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool quit = false;
+            int health = 100;
+            Console.WriteLine("\nYour Health Is : {0}\n", health);
+            Console.WriteLine("Press A To Attack");
+            Console.WriteLine("Press D To Defend");
+            Console.WriteLine("Press R To Run");
+            Console.WriteLine("Press Q To Quit");
+            string input = Console.ReadLine();
+
+            while (quit == false)
+            {
+                switch (input)
+                {
+                    case "A":
+                        Console.WriteLine("You Attacked");
+                        quit = true;
+                        quit = false;
+                        break;
+                    case "D":
+                        Console.WriteLine("You Defended");
+                        quit = true;
+                        break;
+                    case "R":
+                        Console.WriteLine("You Ran");
+                        quit = true;
+                        break;
+                    case "Q":
+                        quit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter A Valid Input");
+                        break;
+                }
+
+
+                if (quit == true)
+                {
+                    Console.Read();
+                }
+            }
+        } 
+    }
+}
